3d665fecaa0e7a2008c9d9f3e9456d9d20f3b0b0
freetimezp <kraftukrnet@ukr.net>
2023-11-15 20:46:58 +0200

    14. js-portfolio-32-web-dev. Contact component. Form validation.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactForm.css';
 
 function ContactForm() {
+    const initialState = {
+        fullname: '',
+        email: '',
+        subject: '',
+        message: '',
+        result: '',
+    };
+
+    const [text, setText] = useState(initialState);
+
+    const changeText = (e) => {
+        const { name, value } = e.target;
+        setText({ ...text, [name]: value, result: '' });
+        //console.log(text);
+    };
+
+    const handleSubmitMessage = (e) => {
+        e.preventDefault();
+        if (text.fullname === '' || text.message === '' || text.email === '' || text.subject === '') {
+            setText({ ...text, result: 'incomplete' })
+        }
+    };
+
     return (
-        <form className='mt-4 contactForm'>
+        <form className='mt-4 contactForm' onSubmit={handleSubmitMessage}>
             <div className="row">
                 <div className="col-md-6 form-group">
-                    <input type="text" name="fullname" className='form-control' id="name" placeholder='Your Name' />
+                    <input
+                        type="text" onChange={changeText} value={text.fullname}
+                        name="fullname" className='form-control' id="name" placeholder='Your Name' />
                 </div>
                 <div className="col-md-6 form-group mt-3 mt-md-0">
                     <input
-                        type="email"
+                        type="email" onChange={changeText} value={text.email}
                         name="email" className='form-control' id="email" placeholder='Your Email' />
                 </div>
             </div>
             <div className="form-group mt-3">
                 <input
-                    type="text"
+                    type="text" onChange={changeText} value={text.subject}
                     name="subject" className='form-control' id="subject" placeholder='Subject' />
             </div>
             <div className="form-group mt-3">
-                <textarea name="message" id="message" rows="6" className='form-control' placeholder='Message'></textarea>
+                <textarea onChange={changeText} value={text.message}
+                    name="message" id="message" rows="6" className='form-control' placeholder='Message'></textarea>
             </div>
+            {text.result === 'incomplete' && (
+                <div className='error-message'>Please,  fill all fields in form..</div>
+            )}
             <div className="text-center">
                 <button type="submit">Send Message</button>
             </div>
